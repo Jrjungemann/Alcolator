8799d730ef2375078c00b9f798f04b13d7a84444
Jonathan Jungemann <jrjungemann@gmail.com>
2014-09-29 16:26:17 -0500

    Initial Commit

---
+++ Calcahol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcahol
+//
+//  Created by Jon Jungemann on 9/29/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcahol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcahol
+//
+//  Created by Jon Jungemann on 9/29/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
